428a7463a1c96df2315cc5c49cbca80ef4e6fdba
Razvan Rujoiu <rjrazvan@icloud.com>
2023-08-27 19:04:31 +0300

    made LoadingState equatable

--- FashionDays/Common/Networking/LoadingState.swift
+++ FashionDays/Common/Networking/LoadingState.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
-enum LoadingState {
+enum LoadingState: Equatable {
     case not_available
     case loading
     case success
     case failed(error: Error)
+    
+    static func == (lhs: LoadingState, rhs: LoadingState) -> Bool {
+        switch (lhs, rhs) {
+        case (.loading, .loading):
+            return true
+        case (.not_available, .not_available):
+            return true
+        case (.success, .success):
+            return true
+        case (.failed(error: let lhsType), .failed(error: let rhsType)):
+            return lhsType.localizedDescription == rhsType.localizedDescription
+        default:
+            return false
+        }
+    }
 }
